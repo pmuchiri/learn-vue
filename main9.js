e2adb9569979bdaba2e1d89a00efd33e2e78f80d
pmuchiri <pmuchiri@cytonn.com>
2020-01-06 13:16:52 -0500

    laracast lessons index files 9-11

--- main9.js
+++ main9.js
@@ -1,32 +1,37 @@
-<<<<<<< HEAD
-Vue.component('task', {
-    // template: '<li>Foobar</li>'
-    template: '<li><slot></slot></li>',
-data() {
-    return {
-        message: 'Vision 2020'
-    };
-}
+Vue.component('message', {
+    props: ['title', 'body'],
 
+    data() {
+        return {
+            isVisible: true
+        };
+    },
+    
+    template: `
+        <article class="message" v-show="isVisible">
+            <div class="message-header">
+                {{ title }}
+
+
+                <!-- <button type="button" @click="hideModal">x</button> -->
+                <button type="button" @click="isVisible = false">x</button>
+            </div>
+
+            <div class="message-body">
+                {{ body }}
+            </div>
+        </article>
+
+    `
+
+    // methods: {
+    //     hideModal() {
+    //         this.isVisible = false;
+    //     }
+    // }
 });
 
 new Vue({
     el: '#root'
 
-=======
-Vue.component('task', {
-    // template: '<li>Foobar</li>'
-    template: '<li><slot></slot></li>',
-data() {
-    return {
-        message: 'Vision 2020'
-    };
-}
-
-});
-
-new Vue({
-    el: '#root'
-
->>>>>>> a584945ed6e6a68eb058163ca4b7cbd627a42c2f
 });
